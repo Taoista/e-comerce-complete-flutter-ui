dd31734df2b230662fd3dcee4a321929b9301c1b
taoista <taoista-@hotmail.com>
2024-02-16 17:29:00 -0300

    respaldando

---
+++ lib/screens/home/components/banners_ofertas.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class BannersOfertas extends StatelessWidget {
+  const BannersOfertas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
